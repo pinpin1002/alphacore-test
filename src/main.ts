03e87e7fb915bfef504affdf29643700dafb86ad
Ivan <pinpin1002@hotmail.com>
2023-08-07 21:55:09 +0800

    Add | quasar

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import "./assets/main.scss";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div>home</div>
+	<div>
+		<q-btn color="white" text-color="black" label="Standard" />
+		<q-btn color="primary" label="Primary" />
+		<q-btn color="secondary" label="Secondary" />
+		<q-btn color="amber" glossy label="Amber" />
+		<q-btn color="brown-5" label="Brown 5" />
+		<q-btn color="deep-orange" glossy label="Deep Orange" />
+		<q-btn color="purple" label="Purple" />
+		<q-btn color="black" label="Black" />
+	</div>
 </template>
 
 <style scoped></style>
